466183e5a0ebb77b79ce75b5d438f9d2208a2e36
Sarah Seagraves <67175494+seagraving@users.noreply.github.com>
2020-12-19 02:38:25 -0600

    separated and edited components, added styling

--- src/App.js
+++ src/App.js
@@ -1,61 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import Container from "./components/container"
-import {Component} from "react"
-import api from "./utils/api"
-import Table from "./components/table"
-import SearchBox from './components/search/search-box'
-import Navbar from './components/navbar';
+import React from "react";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
 
-class App extends Component {
-  state = {
-    employees: [],
-    searchField: "",
-    originalEmployees: []
-  }
-  componentDidMount() {
-    api.getRandomPeople().then(peoples => {
-      this.setState({
-        employees: peoples.data.results,
-        originalEmployees: peoples.data.results
-      })
-    })
-  }
+import "./App.css";
 
-  handleChange = e => {
-    const {name, value} = e.target
-    this.setState({
-      [name]:value
-    })
-    console.log(value)
-
-    const newEmployees = this.state.originalEmployees.filter(person => {
-      return person.name.first.toLowerCase().includes(value.toLowerCase())
-    })
-    this.setState({
-      employees: newEmployees
-    })
-
-  }
-
-  render () {
-    // const {employees} = this.state
-    // const filterName = employees.filter(name =>(
-    //   employees.Employees.toLowerCase().includes(searchField.toLowerCase())
-    // ))
-
-    return (
-      <Container class= "container" >
-        <Navbar handleChange= {this.handleChange}  searchField={this.state.searchField}/>
-        {/* <SearchBox placeholder="Enter Employee Name" handleChange={(e) => this.setState({searchField:e.target.value})} /> */}
-        <Table employees= {this.state.employees} />
-      </Container>
-    
-   );
-  }
-
-  
-
+function App() {
+  return (
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
+    </div>
+  );
 }
 
 export default App;
+
+
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import DataArea from "./DataArea";
+// import "../style/Main.css";
+
+export default class Main extends Component {
+  render() {
+    return (
+      <>
+        <DataArea />
+      </>
+    );
+  }
+}
